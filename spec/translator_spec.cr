5e6cdf93a916e155d50f306a6a416a9b7d104d7d
kojix2 <2xijok@gmail.com>
2024-03-31 14:54:03 +0900

    Add guess_target_language method to Translator

--- spec/translator_spec.cr
+++ spec/translator_spec.cr
@@ -6,6 +6,7 @@ describe DeepL::Translator do
     ENV["DEEPL_AUTH_KEY"] = dummy_api_key
     t = DeepL::Translator.new
     t.auth_key.should eq(dummy_api_key)
+    ENV.delete("DEEPL_AUTH_KEY")
   end
 
   it "can set deepl api key" do
@@ -17,5 +18,17 @@ describe DeepL::Translator do
   it "has default user agent" do
     t = DeepL::Translator.new
     t.user_agent.should eq("deepl.cr/#{DeepL::VERSION}")
+  end
+
+  it "can guess target language from environment variable" do
+    ENV["DEEPL_TARGET_LANGUAGE"] = "CRYSTAL"
+    t = DeepL::Translator.new
+    t.guess_target_language.should eq("CRYSTAL")
+    ENV.delete("DEEPL_TARGET_LANGUAGE")
+  end
+
+  it "can guess target language" do
+    t = DeepL::Translator.new
+    t.guess_target_language.should be_a(String)
   end
 end
--- src/deepl/translator.cr
+++ src/deepl/translator.cr
@@ -299,5 +299,22 @@ module DeepL
       return str unless STDERR.tty?
       "#{"\e[2K\r" if STDERR.tty?}" + str
     end
+
+    def guess_target_language : String
+      tl = ENV["DEEPL_TARGET_LANGUAGE"]?
+      return tl if tl
+      # The language of the current locale
+      # If the locale is de_DE.UTF-8, then the target language is DE
+      {% if flag?(:darwin) || flag?(:unix) %}
+        ENV["LANG"]?.try &.split("_").try &.first.upcase || "EN"
+      {% elsif flag?(:windows) %}
+        l = `powershell -Command "[System.Globalization.CultureInfo]::CurrentCulture.TwoLetterISOLanguageName"`
+        l.empty? ? "EN" : l.strip.upcase
+      {% else %}
+        # From official deepl documentation, EN is deprecated. 
+        # EN-US or EN-GB is recommended.
+        "EN"
+      {% end %}
+    end
   end
 end
