19ab8f0e90b6f17f964e34a6e5540641b42ec43a
kojix2 <2xijok@gmail.com>
2024-07-17 00:57:12 +0900

    Fix GlossaryInfo creation_time type

---
+++ spec/glossary_info_spec.cr
@@ -0,0 +1,24 @@
+require "./spec_helper"
+
+describe DeepL::GlossaryInfo do
+  it "can be serialized to JSON" do
+    tm = Time.utc
+    info = DeepL::GlossaryInfo.new("glossary_id", "Glossary", true, "en", "fr", tm, 10_u32)
+    json = info.to_json
+    json.should eq(%({"glossary_id":"glossary_id","name":"Glossary","ready":true,"source_lang":"en","target_lang":"fr","creation_time":"#{tm.to_rfc3339}","entry_count":10}))
+  end
+
+  it "can be deserialized from JSON" do
+    tm = Time.utc.to_rfc3339
+    json = %({"glossary_id":"glossary_id","name":"Glossary","ready":true,"source_lang":"en","target_lang":"fr","creation_time":"#{tm}","entry_count":10})
+    info = DeepL::GlossaryInfo.from_json(json)
+    info.glossary_id.should eq("glossary_id")
+    info.name.should eq("Glossary")
+    info.ready.should eq(true)
+    info.source_lang.should eq("en")
+    info.target_lang.should eq("fr")
+    info.creation_time.should eq(Time.parse_iso8601(tm))
+    info.entry_count.should eq(10)
+    info.to_json.should eq(json)
+  end
+end
--- src/deepl/glossary_info.cr
+++ src/deepl/glossary_info.cr
@@ -7,7 +7,7 @@ module DeepL
     property ready : Bool
     property source_lang : String
     property target_lang : String
-    property creation_time : String # FIXME
+    property creation_time : Time
     property entry_count : UInt32
 
     def initialize(@glossary_id, @name, @ready, @source_lang, @target_lang, @creation_time, @entry_count)
