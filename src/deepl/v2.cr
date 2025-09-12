39b3f74ff897908ef25a8a2904f134120fa4c2da
kojix2 <2xijok@gmail.com>
2025-09-12 22:03:12 +0900

    Implement glossary ID resolution methods for v2 and v3

--- src/deepl/v2.cr
+++ src/deepl/v2.cr
@@ -9,3 +9,11 @@ require "./v2/glossary"
 require "./v2/usage"
 require "./v2/language"
 require "./v2/rephrase"
+
+module DeepL
+  class Translator
+    def resolve_glossary_id_from_name(name : String) : String
+      find_glossary_info_by_name(name).glossary_id
+    end
+  end
+end
--- src/deepl/v3.cr
+++ src/deepl/v3.cr
@@ -11,3 +11,11 @@ require "./v2/rephrase"
 
 # V3 specific modules
 require "./v3/glossary"
+
+module DeepL
+  class Translator
+    def resolve_glossary_id_from_name(name : String) : String
+      find_multilingual_glossary_by_name(name).glossary_id
+    end
+  end
+end
